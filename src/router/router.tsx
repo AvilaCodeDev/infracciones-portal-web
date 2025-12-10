d55ee7ebb5292212d86c7cb2ae3f1ec83e88056b
Marco Avila <marco.aviladev@gmail.com>
2025-12-09 20:47:57 -0600

    Login

---
+++ src/router/router.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom"
+
+
+import LoginPage from "../modules/auth/pages/LoginPage"
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <LoginPage />
+    }
+])
