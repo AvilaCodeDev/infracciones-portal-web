7164cd352c455c4b2c88cac87f2e6540bf8c8184
Marco Avila <marco.aviladev@gmail.com>
2025-12-10 22:13:33 -0600

    Login Implementation

--- src/modules/auth/functions/verificaIngreso.ts
+++ src/modules/auth/functions/verificaIngreso.ts
@@ -3,6 +3,7 @@ import type { LoginFormValues } from "../interfaces";
 
 export const verficaIngreso = async (data: LoginFormValues) => {
     try {
+        console.log(import.meta.env.VITE_NODE_API);
         return await nodeApi.post("/auth/login", data);
     } catch (error) {
         console.log(error);
